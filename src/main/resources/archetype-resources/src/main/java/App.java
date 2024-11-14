4c22bfb9e6e615fa1b3dbe80cf64dfe6fd00e424
Ruymán García Martín <alu0101408866@ull.edu.es>
2024-11-14 10:58:52 +0000

    Modificando El main

--- src/main/resources/archetype-resources/src/main/java/App.java
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -9,5 +9,8 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        int a = 5
+        int b = 5
+        System.out.println("5 + 5 = " + (a+b));
     }
 }
